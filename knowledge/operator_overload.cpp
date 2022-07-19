29d7bb3b0bd4a6943810c1eefb9c51b4beac1238
luzhixing12345 <luzhixing12345@163.com>
2022-07-19 23:23:52 +0800

    [docs] : add io and overload

---
+++ knowledge/operator_overload.cpp
@@ -0,0 +1,14 @@
+/*
+ *Copyright (c) 2022 All rights reserved
+ *@description: operator overload example
+ *@author: Zhixing Lu
+ *@date: 2022-07-19
+ *@email: luzhixing12345@163.com
+ *@Github: luzhixing12345
+*/
+
+//有关运算符重载的相关概念和知识点请阅读README文件,这里仅提供代码实例和使用方法
+
+
+#include <iostream>
+
